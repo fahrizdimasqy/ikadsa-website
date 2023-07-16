ec864995f12eaef783a3dca0fdd4718646c2f0f2
Fahriz Dimasqy <1197050037@student.uinsgd.ac.id>
2023-07-16 17:28:21 +0700

    add icon font aweasome

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
-const app = createApp(App)
+/* add icons to the library */
+library.add(faUserSecret, faWhatsapp)
+
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
